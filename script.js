f1caa57aedf6bbdf99dcb802fce6286aaa7fd401
JuliannaRkg544 <juliana.rkg544@gmail.com>
2022-04-25 18:20:47 -0300

    projeto finaolizado, bônus 70% feito

--- script.js
+++ script.js
@@ -9,19 +9,20 @@ let cont = 0;
 function selectFood(item,desc, price){
 
     diselectOption('food')
-    console.log('me clicaram', item);
+
+
     item.classList.add("selected");
 
     foodP = price;
     food = desc;
     cont++
-
-    const icon = document.querySelector(".check");
-    item.classList.contains("selected") ? 
-    icon.classList.remove("hidden") :
-    icon.classList.add("hidden")
+    console.log("contador", cont)
+    // const icon = document.querySelector(".check");
+    // item.classList.contains("selected") ? 
+    // icon.classList.remove("hidden") :
+    // icon.classList.add("hidden")
    
-    orderFood()
+    realeaseButton()
 
     
 }
@@ -31,19 +32,19 @@ function selectFood(item,desc, price){
 function selectDrink(item,desc, price){
     diselectOption('drink')
 
-    console.log('me clicaram', item);
     item.classList.add("selected");
 
     drinkP = price;
     drink = desc;
     cont++
+    console.log("contador", cont)
 
-    const icon = document.querySelector(".check");
-    item.classList.contains("selected") ? 
-    icon.classList.remove("hidden") :
-    icon.classList.add("hidden")
+    // const icon = document.querySelector(".check");
+    // item.classList.contains("selected") ? 
+    // icon.classList.remove("hidden") :
+    // icon.classList.add("hidden")
 
-    orderFood()
+    realeaseButton()
 
     
 }
@@ -51,17 +52,17 @@ function selectDrink(item,desc, price){
 function selectDessert(item,desc, price){
     diselectOption('dessert')
 
-    console.log('me clicaram', item);
+   
     item.classList.add("selected");
+
 
     dessertP = price;
     dessert = desc;
     cont++
 
-    const icon = document.querySelector(".check");
-    icon.classList.remove("hidden") 
+    console.log("contador", cont)
 
-    orderFood()
+    realeaseButton()
 
     
 }
@@ -69,19 +70,51 @@ function diselectOption(option){
     //verfifica se tem selected
     //disseleciona se tiver
     //decrementa o cont
+   let product = document.querySelector(`.${option} .selected`);
+   if (product!==null){
+       product.classList.remove("selected");
+       cont--
+   } 
+    console.log("contador", cont);
+}
+
+function realeaseButton(){
+    //verifica se tenho tres items selecionados
    
-    item.classList.add("selected");
-
-    foodP = price;
-    food = desc;
-    cont--
-    
-    
-    
-}
-
-function orderFood(){
+    const button = document.querySelector("footer button")
     if (cont === 3){
+      button.classList.add("released")
+      button.innerHTML = `Fechar Pedido`
+      button.disabled = false;
 
-    }
-}
+   }
+ }
+
+ function orderFood(){
+    console.log('clicado')
+    reviewOrder()
+  
+ }
+ 
+ // const linkDaMensagem = montaMensagemWhatsApp();
+//     window.open(linkDaMensagem, '_blank').focus();
+
+ function reviewOrder(){
+
+    document.querySelector("aside").classList.remove("hidden")
+ }
+
+ function montaMensagemWhatsApp() {
+    const numero = "5522999279652";
+    const total = (foodP + drinkP + dessertP).toFixed(2);
+  
+    let mensagem = `Olá, gostaria de fazer o pedido:
+      - Prato: ${food}
+      - Bebida: ${drink}
+      - Sobremesa: ${dessert}
+      Total: R$ ${total}
+    `;
+  
+    mensagem = encodeURIComponent(mensagem);
+    return `https://wa.me/${numero}?text=${mensagem}`;
+  }
